b57fd05822274467d13827ca36cb4aee3097aea1
Stuart Peters <peters2136@gmail.com>
2019-03-18 10:40:13 -0600

    Renamed Form1 to frmMain

--- ThreadedProjectTerm2/ThreadedProjectTerm2/Program.cs
+++ ThreadedProjectTerm2/ThreadedProjectTerm2/Program.cs
@@ -16,7 +16,7 @@ namespace ThreadedProjectTerm2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
