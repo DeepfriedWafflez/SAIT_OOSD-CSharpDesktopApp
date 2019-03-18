431a6c3aa1d4e3055b9c5fc9f6a898a09a4ef749
Stuart Peters <peters2136@gmail.com>
2019-03-18 11:37:19 -0600

    Added TravelExpertsDBConn class

---
+++ ThreadedProjectTerm2/DBClasses/TravelExpertsDBConn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBClasses
+{
+    class TravelExpertsDBConn
+    {
+        public static SqlConnection getDbConnection()
+        {
+            string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog=TravelExperts;Integrated Security=True";
+
+            //create and retrun a new sql connection object
+            return new SqlConnection(connectionString);
+        }
+
+    }
+}
