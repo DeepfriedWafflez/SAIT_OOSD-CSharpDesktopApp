cd7cfc08c6eb0b8dffb73d1b57b5359cd5ca9204
Stuart Peters <peters2136@gmail.com>
2019-03-18 11:39:02 -0600

    Added ProductSupplier class

---
+++ ThreadedProjectTerm2/TravelExpertsClasses/ProductSupplier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertsClasses
+{
+    public class ProductSupplier
+    {
+
+        //properties
+        public int ProductSupplierID { get; set; }
+
+        public int ProductID { get; set; }
+
+        public int SupplierID { get; set; }
+
+        //constructor
+        public ProductSupplier() { }
+
+        public ProductSupplier(int i, int p, int s)
+        {
+            ProductSupplierID = i;
+            ProductID = p;
+            SupplierID = s;
+
+        }
+
+        //methods
+        public override string ToString()
+        {
+            return "ProductSupplier ID: " + ProductSupplierID + "\n" +
+                   "Product ID: " + ProductID + "\n" +
+                   "Product ID: " + SupplierID;
+        }
+
+    }
+}
