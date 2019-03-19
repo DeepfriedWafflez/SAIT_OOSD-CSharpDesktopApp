431f6f393d4d0d1d77ca05be303c4ef8289d8ee0
DeepfriedWafflez <46905184+DeepfriedWafflez@users.noreply.github.com>
2019-03-19 10:19:44 -0600

    Functions built for suppliers form

--- ThreadedProjectTerm2/ThreadedProjectTerm2/frmSuppliers.Designer.cs
+++ ThreadedProjectTerm2/ThreadedProjectTerm2/frmSuppliers.Designer.cs
@@ -28,20 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label supIDLabel;
+            System.Windows.Forms.Label supNameLabel;
+            this.supplierBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.supplierDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.HeaderLabel = new System.Windows.Forms.Label();
+            this.supIDTextBox = new System.Windows.Forms.TextBox();
+            this.supNameTextBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            supIDLabel = new System.Windows.Forms.Label();
+            supNameLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.supplierBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.supplierDataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // supplierBindingSource
+            // 
+            this.supplierBindingSource.DataSource = typeof(TravelExpertsClasses.Supplier);
+            // 
+            // supplierDataGridView
+            // 
+            this.supplierDataGridView.AutoGenerateColumns = false;
+            this.supplierDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.supplierDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.supplierDataGridView.DataSource = this.supplierBindingSource;
+            this.supplierDataGridView.Location = new System.Drawing.Point(35, 145);
+            this.supplierDataGridView.Name = "supplierDataGridView";
+            this.supplierDataGridView.Size = new System.Drawing.Size(501, 199);
+            this.supplierDataGridView.TabIndex = 1;
+            this.supplierDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.supplierDataGridView_CellClick);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "SupID";
+            this.dataGridViewTextBoxColumn1.FillWeight = 75F;
+            this.dataGridViewTextBoxColumn1.HeaderText = "SupID";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 75;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "SupName";
+            this.dataGridViewTextBoxColumn2.FillWeight = 80F;
+            this.dataGridViewTextBoxColumn2.HeaderText = "SupName";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ClearButton);
+            this.panel1.Controls.Add(this.DeleteButton);
+            this.panel1.Controls.Add(this.UpdateButton);
+            this.panel1.Controls.Add(this.AddButton);
+            this.panel1.Controls.Add(supIDLabel);
+            this.panel1.Controls.Add(this.supIDTextBox);
+            this.panel1.Controls.Add(supNameLabel);
+            this.panel1.Controls.Add(this.supNameTextBox);
+            this.panel1.Controls.Add(this.HeaderLabel);
+            this.panel1.Controls.Add(this.supplierDataGridView);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(575, 372);
+            this.panel1.TabIndex = 2;
+            // 
+            // HeaderLabel
+            // 
+            this.HeaderLabel.AutoSize = true;
+            this.HeaderLabel.Location = new System.Drawing.Point(13, 10);
+            this.HeaderLabel.Name = "HeaderLabel";
+            this.HeaderLabel.Size = new System.Drawing.Size(198, 13);
+            this.HeaderLabel.TabIndex = 2;
+            this.HeaderLabel.Text = "Suppliers:       Click on a Cell to Modify it.";
+            // 
+            // supIDLabel
+            // 
+            supIDLabel.AutoSize = true;
+            supIDLabel.Location = new System.Drawing.Point(52, 51);
+            supIDLabel.Name = "supIDLabel";
+            supIDLabel.Size = new System.Drawing.Size(43, 13);
+            supIDLabel.TabIndex = 2;
+            supIDLabel.Text = "Sup ID:";
+            // 
+            // supIDTextBox
+            // 
+            this.supIDTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.supplierBindingSource, "SupID", true));
+            this.supIDTextBox.Location = new System.Drawing.Point(118, 48);
+            this.supIDTextBox.Name = "supIDTextBox";
+            this.supIDTextBox.Size = new System.Drawing.Size(74, 20);
+            this.supIDTextBox.TabIndex = 3;
+            // 
+            // supNameLabel
+            // 
+            supNameLabel.AutoSize = true;
+            supNameLabel.Location = new System.Drawing.Point(211, 51);
+            supNameLabel.Name = "supNameLabel";
+            supNameLabel.Size = new System.Drawing.Size(60, 13);
+            supNameLabel.TabIndex = 4;
+            supNameLabel.Text = "Sup Name:";
+            // 
+            // supNameTextBox
+            // 
+            this.supNameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.supplierBindingSource, "SupName", true));
+            this.supNameTextBox.Location = new System.Drawing.Point(277, 48);
+            this.supNameTextBox.Name = "supNameTextBox";
+            this.supNameTextBox.Size = new System.Drawing.Size(259, 20);
+            this.supNameTextBox.TabIndex = 5;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(44, 99);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(88, 23);
+            this.AddButton.TabIndex = 6;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(170, 99);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(88, 23);
+            this.UpdateButton.TabIndex = 7;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(308, 99);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(88, 23);
+            this.DeleteButton.TabIndex = 8;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(437, 99);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(88, 23);
+            this.ClearButton.TabIndex = 9;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
             // 
             // frmSuppliers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(575, 372);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmSuppliers";
             this.Text = "frmSuppliers";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmSuppliers_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.supplierBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.supplierDataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource supplierBindingSource;
+        private System.Windows.Forms.DataGridView supplierDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label HeaderLabel;
+        private System.Windows.Forms.TextBox supIDTextBox;
+        private System.Windows.Forms.TextBox supNameTextBox;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button AddButton;
     }
 }
--- ThreadedProjectTerm2/ThreadedProjectTerm2/frmSuppliers.cs
+++ ThreadedProjectTerm2/ThreadedProjectTerm2/frmSuppliers.cs
@@ -23,5 +23,36 @@ namespace ThreadedProjectTerm2
             if (this == activeFrmMain.activeFrmSuppliers)
                 activeFrmMain.activeFrmPackages = null;
         }
+
+
+        //adds the input tothe table
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //updates the selected cell with new data
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //deletes the selected cell
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //clears the input fields
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //clicking cell auto fills text boxes
+        private void supplierDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
