42e8757732ffda1fdce99bd862ab98ff1af08f39
DeepfriedWafflez <46905184+DeepfriedWafflez@users.noreply.github.com>
2019-03-22 12:48:10 -0600

    Added fix for double titlestrip on windows forms

--- ThreadedProjectTerm2/ThreadedProjectTerm2/frmMain.Designer.cs
+++ ThreadedProjectTerm2/ThreadedProjectTerm2/frmMain.Designer.cs
@@ -30,12 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.btnAgents = new System.Windows.Forms.ToolStripButton();
-            this.btnSuppliers = new System.Windows.Forms.ToolStripButton();
-            this.btnProducts = new System.Windows.Forms.ToolStripButton();
-            this.btnPackages = new System.Windows.Forms.ToolStripButton();
             this.btnHome = new System.Windows.Forms.ToolStripButton();
+            this.btnPackages = new System.Windows.Forms.ToolStripButton();
+            this.btnProducts = new System.Windows.Forms.ToolStripButton();
+            this.btnSuppliers = new System.Windows.Forms.ToolStripButton();
+            this.btnAgents = new System.Windows.Forms.ToolStripButton();
             this.btnExit = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,51 +51,11 @@
             this.btnSuppliers,
             this.btnAgents,
             this.btnExit});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(865, 35);
+            this.toolStrip1.Size = new System.Drawing.Size(577, 26);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
-            // 
-            // btnAgents
-            // 
-            this.btnAgents.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnAgents.Image = ((System.Drawing.Image)(resources.GetObject("btnAgents.Image")));
-            this.btnAgents.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnAgents.Name = "btnAgents";
-            this.btnAgents.Size = new System.Drawing.Size(77, 32);
-            this.btnAgents.Text = "Agents";
-            this.btnAgents.Click += new System.EventHandler(this.btnAgents_Click);
-            // 
-            // btnSuppliers
-            // 
-            this.btnSuppliers.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnSuppliers.Image = ((System.Drawing.Image)(resources.GetObject("btnSuppliers.Image")));
-            this.btnSuppliers.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnSuppliers.Name = "btnSuppliers";
-            this.btnSuppliers.Size = new System.Drawing.Size(97, 32);
-            this.btnSuppliers.Text = "Suppliers";
-            this.btnSuppliers.Click += new System.EventHandler(this.btnSuppliers_Click);
-            // 
-            // btnProducts
-            // 
-            this.btnProducts.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnProducts.Image = ((System.Drawing.Image)(resources.GetObject("btnProducts.Image")));
-            this.btnProducts.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnProducts.Name = "btnProducts";
-            this.btnProducts.Size = new System.Drawing.Size(93, 32);
-            this.btnProducts.Text = "Products";
-            this.btnProducts.Click += new System.EventHandler(this.btnProducts_Click);
-            // 
-            // btnPackages
-            // 
-            this.btnPackages.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnPackages.Image = ((System.Drawing.Image)(resources.GetObject("btnPackages.Image")));
-            this.btnPackages.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnPackages.Name = "btnPackages";
-            this.btnPackages.Size = new System.Drawing.Size(95, 32);
-            this.btnPackages.Text = "Packages";
-            this.btnPackages.Click += new System.EventHandler(this.btnPackages_Click);
             // 
             // btnHome
             // 
@@ -102,9 +63,49 @@
             this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
             this.btnHome.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(69, 32);
+            this.btnHome.Size = new System.Drawing.Size(50, 23);
             this.btnHome.Text = "Home";
             this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // btnPackages
+            // 
+            this.btnPackages.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnPackages.Image = ((System.Drawing.Image)(resources.GetObject("btnPackages.Image")));
+            this.btnPackages.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPackages.Name = "btnPackages";
+            this.btnPackages.Size = new System.Drawing.Size(69, 23);
+            this.btnPackages.Text = "Packages";
+            this.btnPackages.Click += new System.EventHandler(this.btnPackages_Click);
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnProducts.Image = ((System.Drawing.Image)(resources.GetObject("btnProducts.Image")));
+            this.btnProducts.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Size = new System.Drawing.Size(67, 23);
+            this.btnProducts.Text = "Products";
+            this.btnProducts.Click += new System.EventHandler(this.btnProducts_Click);
+            // 
+            // btnSuppliers
+            // 
+            this.btnSuppliers.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnSuppliers.Image = ((System.Drawing.Image)(resources.GetObject("btnSuppliers.Image")));
+            this.btnSuppliers.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSuppliers.Name = "btnSuppliers";
+            this.btnSuppliers.Size = new System.Drawing.Size(68, 23);
+            this.btnSuppliers.Text = "Suppliers";
+            this.btnSuppliers.Click += new System.EventHandler(this.btnSuppliers_Click);
+            // 
+            // btnAgents
+            // 
+            this.btnAgents.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnAgents.Image = ((System.Drawing.Image)(resources.GetObject("btnAgents.Image")));
+            this.btnAgents.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAgents.Name = "btnAgents";
+            this.btnAgents.Size = new System.Drawing.Size(56, 23);
+            this.btnAgents.Text = "Agents";
+            this.btnAgents.Click += new System.EventHandler(this.btnAgents_Click);
             // 
             // btnExit
             // 
@@ -112,17 +113,29 @@
             this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
             this.btnExit.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(47, 32);
+            this.btnExit.Size = new System.Drawing.Size(34, 23);
             this.btnExit.Text = "Exit";
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(577, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.Visible = false;
+            // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(865, 450);
+            this.ClientSize = new System.Drawing.Size(577, 292);
             this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmMain";
             this.Text = "Travel Experts - Agent Portal";
             this.Load += new System.EventHandler(this.frmMain_Load);
@@ -142,6 +155,7 @@
         private System.Windows.Forms.ToolStripButton btnSuppliers;
         private System.Windows.Forms.ToolStripButton btnAgents;
         private System.Windows.Forms.ToolStripButton btnExit;
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
